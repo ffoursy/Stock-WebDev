7f5e976c3166b4942acbf676677e6c3565c0cb20
japanj <likitpanjamanon.p@gmail.com>
2018-05-05 22:23:09 +0700

    login.php new

--- login.php
+++ login.php
@@ -8,16 +8,30 @@
   {
     $Username = mysqli_real_escape_string($con,$_POST['username_log']);
     $Userpassword = mysqli_real_escape_string($con,$_POST['password_log']);
-    $result = mysqli_query($con,"SELECT username
+    $result = mysqli_query($con,"SELECT username,user_role
             FROM personal_data
-            WHERE password = $Userpassword AND username = '$Username'") or die("Error: ".mysqli_error($con));
+            WHERE password='$Userpassword' AND username='$Username'") or die("Error: ".mysqli_error($con));
     $row = mysqli_fetch_array($result);
     if(mysqli_num_rows($result)==1)
     {
-      $_SESSION['username'] = $row['username'];
-      $_SESSION['login'] = 1;
-      mysqli_close($con);
-      header("location: trading.php");
+      if ($row['user_role']=="broker") {
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['login']=1;
+        mysqli_close($con);
+        header("location: brokerFirst_select.php");
+      }
+      elseif ($row['user_role']=="user") {
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['login']=1;
+        mysqli_close($con);
+        header("location: trading.php");
+      }
+      elseif ($row['user_role']== "admin") {
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['login']=1;
+        mysqli_close($con);
+        header("location: adminFirst.php");
+      }
     }
     else {
       header("location: first.php?wrongValue=1&wrongText=Your username or password is wrong");
